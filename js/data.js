de797254cc18f5bc78270f83ee5f994f1099f7e4
Swadhin <nepaliswadhin@gmail.com>
2015-11-29 19:58:29 +0545

    Site revamped

---
+++ js/data.js
@@ -0,0 +1,182 @@
+var data = {
+	"Daily Mail" : {
+		"icon":"daily_mail.png",
+		"links":[
+			"http://www.dailymail.co.uk/indiahome/indianews/article-3259672/It-s-time-leave-Nepal-alone.html"
+		]
+	},
+	"New York Times" : {
+		"icon":"nytimes.png",
+		"links":[
+			"http://www.nytimes.com/1989/04/11/world/nepal-s-economy-is-gasping-as-india-a-huge-neighbor-squeezes-it-hard.html",
+			"http://www.nytimes.com/2015/10/12/world/asia/nepal-elects-kp-sharma-oli-as-new-prime-minister.html",
+			"http://nytimes.com/2015/11/07/opinion/nepal-china-india-dragon-elephant-blockade.html",
+			"http://kristof.blogs.nytimes.com/2015/10/13/india-dampens-nepals-celebrations"
+		]
+	},
+	"NDTV News":{
+		"icon":"ndtv.png",
+		"links": [
+			"http://www.ndtv.com/opinion/after-pakistan-modi-isolates-another-neighbour-1223237"
+		]
+	},
+	"The Hindu": {
+		"icon":"the_hindu.png",
+		"links" : [
+			"http://www.thehindu.com/opinion/lead/kanak-mani-dixit-writes-on-nepals-historic-constitution-apply-panchsheel-on-nepal/article7681981.ece",
+			"http://www.thehindu.com/opinion/op-ed/column-by-nirupama-rao-and-atul-pokharel-on-nepal-political-crisis-its-time-to-be-a-good-neighbour/article7770875.ece"
+		]
+	},
+	"Scroll": {
+		"icon":"scroll.in.png",
+		"links":[
+			"http://scroll.in/article/757547/has-modis-neighbourhood-first-model-forgotten-about-nepal",
+			"http://scroll.in/article/766324/how-indias-economic-blockade-of-nepal-may-cause-more-severe-floods-in-bihar"
+		]
+	},
+	"Nepali Times": {
+		"icon":"nepalitimes.png",
+		"links":[
+			"http://nepalitimes.com/article/from-nepali-press/Blockade-arcade,2632",
+			"http://nepalitimes.com/article/editorial/editorial-proxy-war-blockade-is-not-about-the-constitution-anymore,2637",
+			"http://nepalitimes.com/regular-columns/GUEST-COLUMN/india-locked-nepal-is-a-victim-of-india-flexing-muscles,589",
+			"http://nepalitimes.com/regular-columns/As-It-Happens/not-about-constitution-about-inclusion,583",
+			"http://nepalitimes.com/regular-columns/One-To-Many/unexpected-move-at-crucial-time,581",
+			"http://nepalitimes.com/regular-columns/backside/unofficial-alleged-blockade-by-india,580",
+			"http://nepalitimes.com/regular-columns/This/movement-no-longer-about-what-madhesis-want,600"
+		]
+	},
+	"BBC": {
+		"icon":"bbc.png",
+		"links": [
+			"http://bbc.com/news/world-asia-34469494"
+		]
+	},
+	"Indian Express": {
+		"icon":"IndianExpress.ico",
+		"links": [
+			"http://indianexpress.com/article/explained/a-new-pm-for-nepal-power-will-trump-principles",
+			"http://indianexpress.com/article/opinion/columns/big-brothers-blockade",
+			"http://indianexpress.com/article/opinion/columns/raja-mandala-stumbling-in-the-neighbourhood"
+		]
+	},
+	"International Affairs": {
+		"icon": "InternationalAffairs.png",
+		"links": [
+			"http://internationalaffairs.org.au/australian_outlook/indias-demands-grind-nepal-to-a-standstill/"
+		]
+	},
+	"CNN": {
+		"icon":"cnn.png",
+		"links": [
+			"http://edition.cnn.com/2015/09/20/asia/nepal-new-constitution-explainer/"
+		]
+	},
+	"News Laundry": {
+		"icon":"newslaundry.png",
+		"links": [
+			"http://newslaundry.com/2015/10/07/nepals-new-constitution-india-sees-its-neighbours-through-the-narrow-lens-of-security/"
+		]
+	},
+	"Huffington Post": {
+		"icon":"huffington.ico",
+		"links": [
+			"http://huffingtonpost.com/subhash-ghimire/indian-economic-blockade-_b_8259046.html"
+		]
+	},
+	"First Post": {
+		"icon":"firstpost.ico",
+		"links": [
+			"http://firstpost.com/world/still-smiling-and-uncomplaining-a-glimpse-of-life-in-nepal-after-indias-unofficial-blockade-2491374.html"
+		]
+	},
+	"Business Standard": {
+		"icon":"businessStandard.jpeg",
+		"links": [
+			"http://business-standard.com/article/pti-stories/fuel-shortages-may-ground-nepal-s-airlines-official-115110601044_1.html"
+		]
+	},
+	"Daily O": {
+		"icon":"dailyo.ico",
+		"links": [
+			"http://dailyo.in/politics/india-nepal-ties-china-ioc-nepal-oil-corporation-tibet-narendra-modi-petroleum/story/1/7198.html"
+		]
+	},
+	"Deccan Herald": {
+		"icon":"noicon.png",
+		"links": [
+			"http://deccanherald.com/content/506646/indias-costly-policy-failure-nepal.html"
+		]
+	},
+	"The Guardian": {
+		"icon":"noicon.png",
+		"links": [
+			"http://theguardian.com/global-development/2015/nov/06/nepal-government-humanitarian-crisis-india-border-blockade-deputy-prime-minister-kamal-thapa"
+		]
+	},
+	"Nepal Embassy": {
+		"icon":"noicon.png",
+		"links": [
+			"http://nepalembassy.in/Constitution-of-Nepal-2015.pdf",
+			"http://nepalembassy.in/press-06-2015.html"
+		]
+	},
+	"SAAPE": {
+		"icon":"noicon.png",
+		"links": [
+			"http://saape.org/index.php/news-room/press-releases/58-people-s-saarc-india-congratulates-the-people-of-nepal-on-adopting-constitution-of-nepal-2072-and-condemns-modi-government-s-unofficial-blockade-of-nepal"
+		]
+	},
+	"Setopati": {
+		"icon":"noicon.png",
+		"links": [
+			"http://setopati.net/opinion/9563/Why-int’l-community-isn’t-speaking-up-against-Indian-blockade/",
+			"http://setopati.net/opinion/9598/",
+			"http://setopati.net/politics/10021/",
+			"http://setopati.net/opinion/10097/"
+		]
+	},
+	"South Asia": {
+		"icon":"noicon.png",
+		"links": [
+			"http://www.southasia.com.au/2015/09/27/india-has-undeclared-blockade-on-everything-except-on-banana-nepal-pm-koirala/"
+		]
+	},
+	"The Quint": {
+		"icon":"noicon.png",
+		"links": [
+			"http://thequint.com/opinion/2015/10/13/indias-nepal-mess-result-of-a-mix-of-arrogance-and-ineptitude#.Vh3XBhlVJK4.twitter"
+		]
+	},
+	"The Statesman": {
+		"icon":"noicon.png",
+		"links": [
+			"http://thestatesman.com/news/india/nepal-blockade-linked-to-bihar-votes-says-katju/101369.html"
+		]
+	},
+	"Times of India": {
+		"icon":"noicon.png",
+		"links": [
+			"http://timesofindia.indiatimes.com/india/Indian-blockade-more-inhuman-than-war-says-Nepal-PM-KP-Sharma-Oli/articleshow/49690974.cms"
+		]
+	},
+	"Tribune India": {
+		"icon":"noicon.png",
+		"links": [
+			"http://tribuneindia.com/news/comment/how-nepal-was-lost/143371.html"
+		]
+	},
+	"Turkey Agenda": {
+		"icon":"noicon.png",
+		"links": [
+			"http://www.turkeyagenda.com/indian-interference-in-nepal-is-hegemonic-not-moral-2911.html"
+		]
+	},
+	"Zameer36": {
+		"icon":"noicon.png",
+		"links": [
+			"http://zameer36.com/what-india-loses-by-pushing-nepal-into-crisis/"
+		]
+	}
+
+}
