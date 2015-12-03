ba9f391be8b6cfe4bf32c278107abda1a2e1fd5f
Swadhin <nepaliswadhin@gmail.com>
2015-12-03 14:51:20 +0545

    Link Names Added

--- js/data.js
+++ js/data.js
@@ -1,12 +1,22 @@
 var data = {
 	"Daily Mail" : {
 		"icon":"daily_mail.png",
+		"title": [
+			"It's Time to Leave Nepal Alone"
+		],
 		"links":[
 			"http://www.dailymail.co.uk/indiahome/indianews/article-3259672/It-s-time-leave-Nepal-alone.html"
 		]
 	},
 	"New York Times" : {
 		"icon":"nytimes.png",
+		"title": [
+			"Nepal's Economy is Gasping as India A huge Neighbour Squeezes it hard",
+			"Nepal Elects KP Sharma Oli as New Prime Minister",
+			"Nepal China India Dragon Elephant Blockade",
+			"India Dampens Nepal's Celebrations",
+			"Nepal Rations Fuel as Political Crisis with India Worsens"
+		],
 		"links":[
 			"http://www.nytimes.com/1989/04/11/world/nepal-s-economy-is-gasping-as-india-a-huge-neighbor-squeezes-it-hard.html",
 			"http://www.nytimes.com/2015/10/12/world/asia/nepal-elects-kp-sharma-oli-as-new-prime-minister.html",
@@ -17,18 +27,28 @@ var data = {
 	},
 	"DW": {
 		"icon":"dw.png",
+		"title": [
+			"How Protests and Blockade are crippling Nepalese Economy"
+		],
 		"links": [
 			"http://www.dw.com/en/how-protests-and-a-blockade-are-crippling-nepals-economy/a-18875093"
 		]
 	},
 	"NDTV News":{
 		"icon":"ndtv.png",
+		"title": [
+			"After Pakistan Modi Isolates another Neighbour"
+		],
 		"links": [
 			"http://www.ndtv.com/opinion/after-pakistan-modi-isolates-another-neighbour-1223237"
 		]
 	},
 	"The Hindu": {
 		"icon":"the_hindu.png",
+		"title": [
+			"Apply Panchsheel on Nepal",
+			"It's Time to be a good Neighbour"
+		],
 		"links" : [
 			"http://www.thehindu.com/opinion/lead/kanak-mani-dixit-writes-on-nepals-historic-constitution-apply-panchsheel-on-nepal/article7681981.ece",
 			"http://www.thehindu.com/opinion/op-ed/column-by-nirupama-rao-and-atul-pokharel-on-nepal-political-crisis-its-time-to-be-a-good-neighbour/article7770875.ece"
@@ -36,6 +56,11 @@ var data = {
 	},
 	"Scroll": {
 		"icon":"scroll.in.png",
+		"title": [
+			"Has Modi's Neighbourhood First Model Forgotten About Nepal",
+			"How India's Economic Blockade of Nepal may cause more severe floods in Bihar",
+			"Economic Blockade Akin to Imperial Meddling: Filmkaers hit out at Government over Nepal Crisis"
+		],
 		"links":[
 			"http://scroll.in/article/757547/has-modis-neighbourhood-first-model-forgotten-about-nepal",
 			"http://scroll.in/article/766324/how-indias-economic-blockade-of-nepal-may-cause-more-severe-floods-in-bihar",
@@ -44,6 +69,15 @@ var data = {
 	},
 	"Nepali Times": {
 		"icon":"nepalitimes.png",
+		"title": [
+			"Blockade Arcade",
+			"Proxy War Blockade is not about the Constitution of Nepal Anymore",
+			"India Locked Nepal is a Victim of India flexing muscles",
+			"Not About Constitution About Inclusion",
+			"Unexpected Move at Crucial Time",
+			"Unofficial Alleged Blockade by India",
+			"Movement no longer about what Madhesis want"
+		],
 		"links":[
 			"http://nepalitimes.com/article/from-nepali-press/Blockade-arcade,2632",
 			"http://nepalitimes.com/article/editorial/editorial-proxy-war-blockade-is-not-about-the-constitution-anymore,2637",
@@ -56,6 +90,10 @@ var data = {
 	},
 	"BBC": {
 		"icon":"bbc.png",
+		"title": [
+			"Nepal fuel shortages: Petrol 'donated' to Indian Embassy",
+			"Nepal blockade: Large Kathmandu protest by schoolchildren"
+		],
 		"links": [
 			"http://bbc.com/news/world-asia-34469494",
 			"http://www.bbc.com/news/world-asia-34948097"
@@ -63,6 +101,12 @@ var data = {
 	},
 	"Indian Express": {
 		"icon":"IndianExpress.ico",
+		"title": [
+			"A new PM for Nepal will Trump Principles",
+			"Big Brother Blockade",
+			"Stumbling in the Neighbourhood",
+			"Next Door Nepal: A step too far"
+		],
 		"links": [
 			"http://indianexpress.com/article/explained/a-new-pm-for-nepal-power-will-trump-principles",
 			"http://indianexpress.com/article/opinion/columns/big-brothers-blockade",
@@ -72,30 +116,46 @@ var data = {
 	},
 	"International Affairs": {
 		"icon": "internationalAffairs.png",
+		"title": [
+			"India's Demands Grind Nepal to a Stand Still"
+		],
 		"links": [
 			"http://internationalaffairs.org.au/australian_outlook/indias-demands-grind-nepal-to-a-standstill/"
 		]
 	},
 	"CNN": {
 		"icon":"cnn.png",
+		"title": [
+			"Nepal's new Constitution Explainer"
+		],
 		"links": [
 			"http://edition.cnn.com/2015/09/20/asia/nepal-new-constitution-explainer/"
 		]
 	},
 	"News Laundry": {
 		"icon":"newslaundry.png",
+		"title": [
+			"India Sees its Neighbourhood through the narrow lens of Security"
+		],
 		"links": [
 			"http://newslaundry.com/2015/10/07/nepals-new-constitution-india-sees-its-neighbours-through-the-narrow-lens-of-security/"
 		]
 	},
 	"Huffington Post": {
 		"icon":"huffington.ico",
+		"title": [
+			"Indian Economic Blockade"
+		],
 		"links": [
 			"http://huffingtonpost.com/subhash-ghimire/indian-economic-blockade-_b_8259046.html"
 		]
 	},
 	"First Post": {
 		"icon":"firstpost.ico",
+		"title": [
+			"Still Smiling and Uncomplaining: A Glimpse of life in Nepal after India's unofficial Blockade",
+			"Nepal PM Oli urges India to Lift Economic Blockade, says its causing a Humanitarian Crisis"
+		],
 		"links": [
 			"http://firstpost.com/world/still-smiling-and-uncomplaining-a-glimpse-of-life-in-nepal-after-indias-unofficial-blockade-2491374.html",
 			"http://www.firstpost.com/world/nepal-pm-oli-urges-india-to-lift-economic-blockade-says-its-causing-a-humanitarian-crisis-2507660.html"
@@ -103,18 +163,30 @@ var data = {
 	},
 	"Business Standard": {
 		"icon":"businessStandard.jpeg",
+		"title": [
+			"Fuel Shortages may ground Nepal's Airlines: Officials"
+		],
 		"links": [
 			"http://business-standard.com/article/pti-stories/fuel-shortages-may-ground-nepal-s-airlines-official-115110601044_1.html"
 		]
 	},
 	"Daily O": {
 		"icon":"dailyo.ico",
+		"title": [
+			"How India surrendered its Nepal monopoly to China"
+		],
 		"links": [
 			"http://dailyo.in/politics/india-nepal-ties-china-ioc-nepal-oil-corporation-tibet-narendra-modi-petroleum/story/1/7198.html"
 		]
 	},
 	"Dawn": {
 		"icon":"dawn.png",
+		"title": [
+			"Nepal Facing Medicine Shortages due to Fuel Crisis and Blockade",
+			"India used Economic Blockade to Dictate Nepalese Constitution",
+			"Nepali Leader says India breaking International Law",
+			"UN chief asks India to lift Nepal blockade"
+		],
 		"links": [
 			"http://www.dawn.com/news/1220797/nepal-facing-medicine-shortages-due-to-fuel-crisis-blockade",
 			"http://www.dawn.com/news/1217707/india-used-economic-blockade-to-dictate-nepalese-constitution",
@@ -124,18 +196,28 @@ var data = {
 	},
 	"Deccan Herald": {
 		"icon":"deccan.jpg",
+		"title": [
+			"India's costly policy failure"
+		],
 		"links": [
 			"http://deccanherald.com/content/506646/indias-costly-policy-failure-nepal.html"
 		]
 	},
 	"The Guardian": {
 		"icon":"guardian.png",
+		"title": [
+			"Nepal warns of humanitarian crisis as India border blockade continues"
+		],
 		"links": [
 			"http://theguardian.com/global-development/2015/nov/06/nepal-government-humanitarian-crisis-india-border-blockade-deputy-prime-minister-kamal-thapa"
 		]
 	},
 	"Nepal Embassy": {
 		"icon":"noicon.png",
+		"title": [
+			"Constitution of Nepal 2015",
+			"Press Release 2015"
+		],
 		"links": [
 			"http://nepalembassy.in/Constitution-of-Nepal-2015.pdf",
 			"http://nepalembassy.in/press-06-2015.html"
@@ -143,12 +225,19 @@ var data = {
 	},
 	"SAAPE": {
 		"icon":"saape.ico",
+		"title": ["People’s SAARC-India congratulates the people of Nepal on adopting ‘Constitution of Nepal 2072’ and condemns Modi Government’s unofficial blockade of Nepal"],
 		"links": [
 			"http://saape.org/index.php/news-room/press-releases/58-people-s-saarc-india-congratulates-the-people-of-nepal-on-adopting-constitution-of-nepal-2072-and-condemns-modi-government-s-unofficial-blockade-of-nepal"
 		]
 	},
 	"Setopati": {
 		"icon":"setopati.ico",
+		"title": [
+			"Why International Community isn't speaking up against Indian Blockade",
+			"Indian blockade in Nepal and apathy of UN",
+			"A Mexican standoff in Nepal",
+			"Indian Blockade - Facts and Fiction"
+		],
 		"links": [
 			"http://setopati.net/opinion/9563/Why-int’l-community-isn’t-speaking-up-against-Indian-blockade/",
 			"http://setopati.net/opinion/9598/",
@@ -158,60 +247,90 @@ var data = {
 	},
 	"South Asia": {
 		"icon":"southasia.jpg",
+		"title": [
+			"India has Undeclared Blockade on everything except on Banana: Nepal PM Koirala"
+		],
 		"links": [
 			"http://www.southasia.com.au/2015/09/27/india-has-undeclared-blockade-on-everything-except-on-banana-nepal-pm-koirala/"
 		]
 	},
 	"The Quint": {
 		"icon":"thequint.ico",
+		"title": [
+			"India's Nepal Mess is a result of a mix of Arrogance and Ineptitude"
+		],
 		"links": [
 			"http://thequint.com/opinion/2015/10/13/indias-nepal-mess-result-of-a-mix-of-arrogance-and-ineptitude#.Vh3XBhlVJK4.twitter"
 		]
 	},
 	"The Statesman": {
 		"icon":"statesman.ico",
+		"title": [
+			"Nepal Blockade linked to Bihar votes says Katju"
+		],
 		"links": [
 			"http://thestatesman.com/news/india/nepal-blockade-linked-to-bihar-votes-says-katju/101369.html"
 		]
 	},
 	"Times of India": {
 		"icon":"toi.png",
+		"title": [
+			"Indian Blockade more inhuman than War says Nepal PM KP Sharma Oli"
+		],
 		"links": [
 			"http://timesofindia.indiatimes.com/india/Indian-blockade-more-inhuman-than-war-says-Nepal-PM-KP-Sharma-Oli/articleshow/49690974.cms"
 		]
 	},
 	"Tribune India": {
 		"icon":"tribune.png",
+		"title": [
+			"How Nepal was Lost"
+		],
 		"links": [
 			"http://tribuneindia.com/news/comment/how-nepal-was-lost/143371.html"
 		]
 	},
 	"Turkey Agenda": {
 		"icon":"turkeyAgenda.ico",
+		"title": [
+			"Indian Interference in Nepal is Hegemonic not Moral"
+		],
 		"links": [
 			"http://www.turkeyagenda.com/indian-interference-in-nepal-is-hegemonic-not-moral-2911.html"
 		]
 	},
 	"Time": {
 		"icon":"time.png",
+		"title": [
+			"India and Nepal Have No Choice but to End Their Border Dispute and Move On"
+		],
 		"links": [
 			"http://time.com/4115801/nepal-india-border-blockade-madhesh/"
 		]
 	},
 	"Wall Street Journal": {
 		"icon":"wsj.ico",
+		"title": [
+			"The Two Month Blockade of Nepal explained"
+		],
 		"links" : [
 			"http://blogs.wsj.com/indiarealtime/2015/11/26/the-two-month-blockade-of-nepal-explained/"
 		]
 	},
 	"Washington Post": {
 		"icon":"wp.ico",
+		"title": [
+			"Nepal is Angry with India so it turns off the TV"
+		],
 		"links": [
 			"https://www.washingtonpost.com/news/worldviews/wp/2015/09/29/nepal-is-angry-with-india-so-it-turns-off-the-tv/"
 		]
 	},
 	"Yahoo News": {
 		"icon":"yahoo.ico",
+		"title": [
+			"Nepal Facing Medicine Shortages due to Fuel Crisis and Blockade"
+		],
 		"links": [
 			"http://news.yahoo.com/nepal-facing-medicine-shortages-due-fuel-crisis-blockade-093721301.html"
 		]
